13c3eb4cbd501f404cb40394b13335cb947c7102
chokri samaali <34174245+samalien@users.noreply.github.com>
2020-11-30 17:57:22 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,36 +1,61 @@
 #---------------------------------------------
+#Ecrire une fonction qui demande un nombre de départ, et qui calcule son carré 
 def calcul_carre(x):
     return "Fonction manquante !"
 
+#Ecrire une fonction qui demande un nombre de départ, et qui calcule son cube 
 def calcul_cube(x):
     return "Fonction manquante !"
 
+#Ecrire une fonction qui demande un nombre de départ, et qui calcule sa factorielle.
 def calcul_factoriel(x):
     return "Fonction manquante !"
 
+
+#Ecrire une fonction qui détermine le PGCD (plus grand commun diviseur) de deux nombres naturels a et b.
 def calcul_pgcd(x, y):
     return "Fonction manquante !"
 
 
+
+#Ecrire une fonction qui détermine le PPCM (plus petit commun multiple) de deux nombres naturels a et b.
 def calcul_ppcm(a, b):
     return "Fonction manquante !"
 
 
+"""
+Ecrire une fonction donnant la décomposition en facteurs premier d’un nombre entier. Le résultat doit être sous forme de chaine de caractères (string).
+Principe : 
+•	Vérifier si n est divisible par 2, si oui : remplacer n par n div 2 et continuer à le diviser par 2, jusqu'à ce qu'il ne soit pas multiple de 2 (n mod 2 <>0) 
+•	Refaire l'étape précédente pour 3, 4, … +Refaire les étapes précédentes jusqu'à avoir n = 1
+"""
 def decompose(n):
     return "Fonction manquante !"
 
 
+"""
+Ecrire une fonction qui vérifié la primalité d’un nombre donné
+Un nombre premier est un nombre entier naturel (non nul) qui admet exactement 2 diviseurs distincts : 1 et lui-même.
+"""
 def calcul_premier(x):
     return "Fonction manquante !"
 
+
+#Ecrire un algorithme qui détermine si un entier N positif est parfait ou non. Un nombre est dit parfait s’il est égal à la somme de ses diviseurs. Exemple : 6 = 1+2+3
 def calcul_parfait(x):
     return "Fonction manquante !"
+
+
 
 def calcul_tg(x):
     return "Fonction manquante !"
 
+
+
 def calcul_sin(x):
     return "Fonction manquante !"
+
+
 
 def calcul_cos(x):
     return "Fonction manquante !"
@@ -43,8 +68,12 @@ def bin_dec(x):
     return "Fonction manquante !"
 
 
+
+
 def dec_oct(x):
     return "Fonction manquante !"
+
+
 
 
 def oct_dec(x):
